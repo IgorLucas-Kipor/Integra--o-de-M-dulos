e256a2533c3c9d3774d69bb7daacf4406f7e777d
Igor Lucas <igorescritor@hotmail.com>
2021-05-19 21:29:56 -0300

    Integração do back-end ao mongoDB

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,9 +1,39 @@
 const express = require('express');
-
+const mongodb = require('mongodb');
 const router = express.Router();
 
+
+const uri = "mongodb+srv://vue123:1234@cluster0.ur4ds.mongodb.net/MyFirstDatabase?retryWrites=true&w=majority";
+const client = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// listar todos os posts
 router.get('/', async(req, res) => {
-    res.send('Hello');
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
 })
+
+// criar posts
+router.post('/', async(req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne ({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+})
+
+// apagar um post
+
+router.delete('/:id', async(req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send;
+})
+
+async function loadPostsCollection() {
+    await client.connect();
+    return client.db('vue_express').collection('posts');
+
+}
 
 module.exports = router;
